d76fa526d8654c8e85da7319d00c89d40b3dc52b
hxgreensoul <htrejo42@gmail.com>
2022-08-21 23:53:18 -0700

    First Commit

---
+++ Assets/Scripts/Teleporting.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Teleporting : MonoBehaviour
+{
+    public Transform teleportTarget;
+    public GameObject player;
+    private void OnTriggerEnter(Collider collider)
+    {
+        player.transform.position = teleportTarget.transform.position;
+        Debug.Log("Triggered!");
+    }
+}
---
+++ Assets/Scripts/addTeleport.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addTeleport : MonoBehaviour
+{
+    
+    public Transform teleportTarget; // the variable for the teleport position
+    public GameObject thePlayer; // the variable for the teleporting player
+
+    void onTriggerEnter(Collider other) // setting the trigger for teleportation 
+    {
+        thePlayer.transform.position = teleportTarget.transform.position;// the trigger makes one position equal to another
+        //{ Debug.Log("ENTERED WARP ZONE"); }
+        print("Player has entered teleport pad");
+        Debug.Log("Triggered!");
+    }
+    
+}
